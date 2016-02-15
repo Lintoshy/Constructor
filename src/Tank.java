b6942db8948937766ad44ce232331877242325dd
Lintoshy <lintoshy@gmail.com>
2016-02-15 16:25:54 +0200

    First commit

---
+++ src/Tank.java
@@ -0,0 +1,13 @@
+
+public class Tank {
+	String color;
+	int crew;
+	int maxSpeed;
+	
+	Tank (String color, int crew, int maxSpeed) {
+		this.color = color;
+		this.crew = crew;
+		this.maxSpeed = maxSpeed;
+	}
+
+}
